4b89612a582bf8267611e08404f4e089bca6861d
aswathy <aswathyseb@gmail.com>
2017-09-17 22:13:41 -0400

    files modified

--- standalone-django/standalone.py
+++ standalone-django/standalone.py
@@ -20,6 +20,25 @@ def set_env():
     settings.configure(TEMPLATES=TEMPLATES)
 
 
+def read_data2json(fname):
+    '''
+    reads a config file in the form
+    data1 = val1
+    data2 = val2,val3
+    and returns a json file.
+    :param fname: input file in text format
+    :return: writes a json file
+    '''
+    data = dict()
+    with open(fname) as fh:
+        for stream in fh:
+            param = stream.split("=")
+            vals = param[1].strip().split(",")
+            data[param[0].strip()] = vals if len(vals) >1 else param[1].strip()
+    with open('input.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4, separators=(',', ': '))
+    return
+
 def metadata_loader(fname):
     '''
     reads json file and returns the data
@@ -35,6 +54,7 @@ def metadata_loader(fname):
 if __name__ == '__main__':
     set_env()
     django.setup()
+    read_data2json("input.txt")
     metadata = metadata_loader("input.json")
     template = get_template('igv.html')
     html = template.render(metadata)
